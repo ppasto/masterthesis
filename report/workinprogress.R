478da5517c0ebf9343730cdebe0d6502c54a55ce
Pilar Pastor <pilar.pastormartinez@uzh.ch>
2025-04-24 12:15:18 +0200

    added qgammagamma() to report, chapter 5 results

--- report/workinprogress.R
+++ report/workinprogress.R
@@ -1291,7 +1291,7 @@ pgammagamma <- function(t, alpha, b, c) {
 }
 
 qgammagamma <- function(p, alpha, b, c, tol = 1e-6, max = 100000) {
-	uniroot(function(x) pgammagamma(x, alpha, b, c) - 0.9, c(tol,max))$root
+	uniroot(function(x) pgammagamma(x, alpha, b, c) - p, c(tol,max))$root
 }
 
 pgammagamma(548, alpha = 32.4, b = 54.8, c = 324)
